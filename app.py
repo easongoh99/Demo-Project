c2c47dcd6343244739334f94514d69636479617b
easongoh99 <81219220+easongoh99@users.noreply.github.com>
2026-07-14 06:45:24 +0000

    initiate app.py to Flask App

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World DevOps Project Test"
+
+@app.route("/new")
+def new():
+    return "New Hello World DevOps Project Test"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
